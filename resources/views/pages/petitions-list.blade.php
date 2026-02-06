682f992537c679eefcc4a418b8914a4696210a4e
Abdulzizi <jawadazizi052@gmail.com>
2026-02-06 21:28:54 +0700

    categories fixed

--- resources/views/pages/petitions-list.blade.php
+++ resources/views/pages/petitions-list.blade.php
@@ -8,7 +8,10 @@
 
             <div class="bg-white shadow-sm rounded-3 p-4" style="border:1px solid #eee;">
 
-                <div class="mb-2" style="font-weight:700;">Petitions</div>
+                <div class="mb-2" style="font-weight:700;">
+                    {{ $heading ?? 'Petitions' }}
+                </div>
+
                 <div style="height:2px;background:#e9ecef;position:relative;margin-bottom:22px;">
                     <div
                         style="height:2px;width:100%;background:linear-gradient(to right, black, red);position:absolute;left:0;top:0;">
@@ -17,16 +20,22 @@
 
                 <div class="fc-petitions-list">
                     @forelse($petitions as $petition)
-                        <a href="{{ $petitionUrl($petition) }}" class="fc-petition-row">
-                            {{ $petitionTitle($petition) }}
-                        </a>
+                                    <a href="{{ lroute('petition.show', [
+                            'slug' => $petition->tr_slug,
+                            'id' => $petition->id,
+                        ]) }}" class="fc-petition-row">
+                                        {{ $petition->tr_title }}
+                                    </a>
                     @empty
-                        <div class="text-muted">no petitions found.</div>
+                        <div class="text-muted">
+                            {{ __('No petitions found.') }}
+                        </div>
                     @endforelse
                 </div>
 
                 @include('partials.pagination', ['paginator' => $petitions])
             </div>
+
         </div>
     </section>
 @endsection
