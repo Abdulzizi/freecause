57a2fa959ae3f86d8e7d592c997279e5f9363fc3
Abdulzizi <jawadazizi052@gmail.com>
2026-02-17 14:36:51 +0700

    admin panel dev - fallback to english content if there is no transaltion for static page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\PageTranslation;
-use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
@@ -13,7 +12,15 @@ class PageController extends Controller
             ->where('locale', $locale)
             ->where('slug', $slug)
             ->where('published', true)
-            ->firstOrFail();
+            ->first();
+
+        if (!$tr) {
+            $tr = PageTranslation::query()
+                ->where('locale', 'en')
+                ->where('slug', $slug)
+                ->where('published', true)
+                ->firstOrFail();
+        }
 
         return view('pages.show', compact('tr', 'locale'));
     }
