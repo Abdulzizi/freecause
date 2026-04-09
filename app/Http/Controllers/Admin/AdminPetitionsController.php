48cf03188876124008154206b1bfb1c4c8125f2f
Abdulzizi <jawadazizi052@gmail.com>
2026-04-09 13:34:18 +0700

    fix: guard against empty safeTitle after BOOLEAN MODE sanitization

--- app/Http/Controllers/Admin/AdminPetitionsController.php
+++ app/Http/Controllers/Admin/AdminPetitionsController.php
@@ -62,10 +62,12 @@ class AdminPetitionsController extends Controller
         if ($filters['title'] !== '') {
             $safeTitle = preg_replace('/[+\-<>~*()"@]+/', ' ', $filters['title']);
             $safeTitle = trim($safeTitle);
-            $q->whereRaw(
-                "MATCH(pt.title) AGAINST(? IN BOOLEAN MODE) OR MATCH(pt_default.title) AGAINST(? IN BOOLEAN MODE)",
-                ['+' . $safeTitle . '*', '+' . $safeTitle . '*']
-            );
+            if ($safeTitle !== '') {
+                $q->whereRaw(
+                    "MATCH(pt.title) AGAINST(? IN BOOLEAN MODE) OR MATCH(pt_default.title) AGAINST(? IN BOOLEAN MODE)",
+                    ['+' . $safeTitle . '*', '+' . $safeTitle . '*']
+                );
+            }
         }
 
         if ($filters['featured'] !== '') {
